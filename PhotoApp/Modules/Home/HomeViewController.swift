c1dff5f90af9ebd6439a49dee34ef35a8f5a2920
arai <niimaru09@gmail.com>
2023-08-24 14:32:54 +0900

    refactor HomeViewController 継承を前提としない

--- PhotoApp/Modules/Home/HomeViewController.swift
+++ PhotoApp/Modules/Home/HomeViewController.swift
@@ -7,27 +7,11 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+final class HomeViewController: UIViewController {
     
-    private var garbageButton: UIBarButtonItem!
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        garbageButton = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(tapGarbage(_:)))
         
-        self.navigationItem.rightBarButtonItem = garbageButton
-
     }
-    
-    @objc private func tapGarbage(_ sender: UIBarButtonItem) {
-        
-        print("hoge")
-        let vc = ReadQRViewController.loadStoryboard()
-        self.present(vc, animated: true)
-
-    }
-    
 
 }
