d600cdbe1c8a55754f0034d1110f2d1304ac8f3e
arai <niimaru09@gmail.com>
2023-08-24 14:31:17 +0900

    refactor SelectInterviewDateViewController, add Constants

--- PhotoApp/Modules/ReadQRViewController/SelectInterviewDate/SelectInterviewDateViewController.swift
+++ PhotoApp/Modules/ReadQRViewController/SelectInterviewDate/SelectInterviewDateViewController.swift
@@ -9,99 +9,85 @@ import UIKit
 import SVProgressHUD
 
 final class SelectInterviewDateViewController: UIViewController {
+    
+    @IBOutlet private weak var scrollView: UIScrollView!
+    @IBOutlet private weak var submitButton: UIButton!
+    @IBOutlet private weak var dateLabel: UILabel!
 
-//    @IBOutlet weak var tableView: UITableView!
+    private var pageViews: [UITableView?] = []
+    private var selectedIndexPath: IndexPath?
+    private var selectedTime = ""
     
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var submitButton: UIButton!
-    var pageViews: [UITableView?] = []
-    
-    var selectedIndexPath: IndexPath?
-    @IBOutlet weak var dateLabel: UILabel!
-    
-    var selectedTime = ""
-    
-
-    
-    private let reservations = [
-        ReservationSlot(time: "9:05 ~ 9:10", isOpen: true),
-        ReservationSlot(time: "9:15 ~ 9:20", isOpen: true),
-        ReservationSlot(time: "9:30 ~ 9:35", isOpen: true),
-//        ReservationSlot(time: "9:45 ~ 9:50", isOpen: true),
-        ReservationSlot(time: "10:05 ~ 10:10", isOpen: true),
-        ReservationSlot(time: "10:15 ~ 10:20", isOpen: true),
-        ReservationSlot(time: "10:30 ~ 10:35", isOpen: true),
-//        ReservationSlot(time: "10:45 ~ 10:50", isOpen: true),
-        ReservationSlot(time: "11:05 ~ 11:10", isOpen: true),
-        ReservationSlot(time: "11:15 ~ 11:20", isOpen: true),
-        ReservationSlot(time: "11:30 ~ 11:35", isOpen: true),
-        ReservationSlot(time: "11:45 ~ 11:50", isOpen: true),
-//        ReservationSlot(time: "12:05 ~ 12:10", isOpen: true),
-//        ReservationSlot(time: "12:15 ~ 12:20", isOpen: true),
-//        ReservationSlot(time: "12:30 ~ 12:35", isOpen: true),
-//        ReservationSlot(time: "12:45 ~ 12:50", isOpen: true),
-        ReservationSlot(time: "13:05 ~ 13:10", isOpen: true),
-        ReservationSlot(time: "13:15 ~ 13:20", isOpen: true),
-//        ReservationSlot(time: "13:30 ~ 13:35", isOpen: true),
-        ReservationSlot(time: "13:45 ~ 13:50", isOpen: true),
-        ReservationSlot(time: "14:05 ~ 14:10", isOpen: true),
-        ReservationSlot(time: "14:15 ~ 14:20", isOpen: true),
-        ReservationSlot(time: "14:30 ~ 14:35", isOpen: true),
-        ReservationSlot(time: "14:45 ~ 14:50", isOpen: true),
-        ReservationSlot(time: "15:05 ~ 15:10", isOpen: true),
-        ReservationSlot(time: "15:15 ~ 15:20", isOpen: true),
-        ReservationSlot(time: "15:30 ~ 15:35", isOpen: true),
-//        ReservationSlot(time: "15:45 ~ 15:50", isOpen: true),
-        ReservationSlot(time: "16:05 ~ 16:10", isOpen: true),
-        ReservationSlot(time: "16:15 ~ 16:20", isOpen: true),
-        ReservationSlot(time: "16:30 ~ 16:35", isOpen: true),
-        ReservationSlot(time: "16:45 ~ 16:50", isOpen: true),
-        ReservationSlot(time: "17:05 ~ 17:10", isOpen: true),
-        ReservationSlot(time: "17:15 ~ 17:20", isOpen: true),
-        ReservationSlot(time: "17:30 ~ 17:35", isOpen: true),
-        ReservationSlot(time: "17:45 ~ 17:50", isOpen: true),
-    ]
-    
-    private let dates = [
-        "2023/08/25",
-        "2023/08/26",
-        "2023/08/27",
-        "2023/08/28",
-        "2023/08/29",
-        "2023/08/30",
-        "2023/08/31",
-        "2023/09/01",
-        "2023/09/02",
-    ]
+    private let reservations = Constants.testReservationsData
+    private let dates = Constants.testDates
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         scrollView.delegate = self
+        setUpViews()
+
+    }
+    
+    private func setUpViews() {
+        
         scrollView.isPagingEnabled = true
-        
-        // ここでページの数を設定する
         let pageCount = dates.count
-        
         for _ in 0..<pageCount {
             pageViews.append(nil)
         }
         
         let pagesScrollViewSize = scrollView.frame.size
         scrollView.contentSize = CGSize(width: pagesScrollViewSize.width * CGFloat(pageCount), height: pagesScrollViewSize.height)
-        
         loadVisiblePages()
         
         submitButton.isEnabled = false
-        
-        
         submitButton.addTarget(nil, action: #selector(showConfirmationAlert), for: .touchUpInside)
-
         submitButton.backgroundColor = .lightGray
-        // Do any additional setup after loading the view.
     }
     
-    func loadPage(_ page: Int) {
+    @objc
+    private func showConfirmationAlert() {
+        let pageWidth = scrollView.frame.size.width
+
+        let currentPage = Int(scrollView.contentOffset.x / pageWidth)
+        let alert = UIAlertController(title: "\(dates[currentPage]) \n\(selectedTime)", message: "この日程でよろしいですか？", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "はい", style: .default) { (action) in
+            self.dismiss(animated: true, completion: nil)
+            
+            self.processReservation()
+
+        }
+        let cancel = UIAlertAction(title: "いいえ", style: .cancel) { (acrion) in
+            self.dismiss(animated: true, completion: nil)
+        }
+        alert.addAction(ok)
+        alert.addAction(cancel)
+
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    private func processReservation() {
+        SVProgressHUD.show(withStatus: "予約処理中")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            SVProgressHUD.showSuccess(withStatus: "予約が確定しました。")
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                SVProgressHUD.dismiss()
+                self.navigationController?.popToRootViewController(animated: true)
+            }
+        }
+    }
+
+}
+
+
+extension SelectInterviewDateViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        loadVisiblePages()
+    }
+    
+    private func loadPage(_ page: Int) {
         if page < 0 || page >= pageViews.count {
             return
         }
@@ -116,7 +102,7 @@ final class SelectInterviewDateViewController: UIViewController {
         }
     }
     
-    func loadVisiblePages() {
+    private func loadVisiblePages() {
         let pageWidth = scrollView.frame.size.width
         let currentPage = Int(scrollView.contentOffset.x / pageWidth)
         
@@ -126,75 +112,23 @@ final class SelectInterviewDateViewController: UIViewController {
         
         dateLabel.text = dates[currentPage]
     }
-    
-    @objc
-    private func showConfirmationAlert() {
-        let pageWidth = scrollView.frame.size.width
-
-        let currentPage = Int(scrollView.contentOffset.x / pageWidth)
-        let alert = UIAlertController(title: "\(dates[currentPage]) \n\(selectedTime)", message: "この日程でよろしいですか？", preferredStyle: .alert)
-        let ok = UIAlertAction(title: "はい", style: .default) { (action) in
-            self.dismiss(animated: true, completion: nil)
-            
-            SVProgressHUD.show(withStatus: "予約処理中")
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                SVProgressHUD.showSuccess(withStatus: "予約が確定しました。")
-
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                    SVProgressHUD.dismiss()
-                    self.navigationController?.popToRootViewController(animated: true)
-                    
-
-                }
-            }
-
-        }
-        let cancel = UIAlertAction(title: "いいえ", style: .cancel) { (acrion) in
-            self.dismiss(animated: true, completion: nil)
-        }
-        alert.addAction(ok)
-        alert.addAction(cancel)
-
-        self.present(alert, animated: true, completion: nil)
-    }
-    
-
 }
-
-
-extension SelectInterviewDateViewController: UIScrollViewDelegate {
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        loadVisiblePages()
-    }
-}
-
-
-    
-
-
-
 
 extension SelectInterviewDateViewController: UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return reservations.count
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 44
     }
-//    func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
-//        false //ハイライトしない
-//    }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: PickDateTableViewCell.className, for: indexPath) as! PickDateTableViewCell
         
         cell.selectionStyle = .none
-        
         cell.dateLabel.text = reservations[indexPath.row].time
-        
-        if !reservations[indexPath.row].isOpen {
-            cell.checkImage.isHidden = true
-        }
         
         if indexPath == selectedIndexPath {
             cell.checkImage.image = UIImage(systemName: "checkmark.circle.fill")
@@ -202,9 +136,6 @@ extension SelectInterviewDateViewController: UITableViewDelegate, UITableViewDat
         } else {
             cell.checkImage.image = UIImage(systemName: "circle")
         }
-        
-
-        
     
         return cell
     }
@@ -215,11 +146,9 @@ extension SelectInterviewDateViewController: UITableViewDelegate, UITableViewDat
         selectedIndexPath = indexPath
         submitButton.isEnabled = true
         submitButton.backgroundColor = .systemTeal
-
         
-        
-
         tableView.reloadData()
+
     }
     
 }
---
+++ PhotoApp/Utility/Constants.swift
@@ -0,0 +1,61 @@
+//
+//  Constants.swift
+//  PhotoApp
+//
+//  Created by arai kousuke on 2023/08/24.
+//
+
+import Foundation
+
+class Constants {
+    static let testReservationsData = [
+        ReservationSlot(time: "9:05 ~ 9:10", isOpen: true),
+        ReservationSlot(time: "9:15 ~ 9:20", isOpen: true),
+        ReservationSlot(time: "9:30 ~ 9:35", isOpen: false),
+//        ReservationSlot(time: "9:45 ~ 9:50", isOpen: true),
+        ReservationSlot(time: "10:05 ~ 10:10", isOpen: true),
+        ReservationSlot(time: "10:15 ~ 10:20", isOpen: true),
+        ReservationSlot(time: "10:30 ~ 10:35", isOpen: true),
+//        ReservationSlot(time: "10:45 ~ 10:50", isOpen: true),
+        ReservationSlot(time: "11:05 ~ 11:10", isOpen: true),
+        ReservationSlot(time: "11:15 ~ 11:20", isOpen: true),
+        ReservationSlot(time: "11:30 ~ 11:35", isOpen: true),
+        ReservationSlot(time: "11:45 ~ 11:50", isOpen: true),
+//        ReservationSlot(time: "12:05 ~ 12:10", isOpen: true),
+//        ReservationSlot(time: "12:15 ~ 12:20", isOpen: true),
+//        ReservationSlot(time: "12:30 ~ 12:35", isOpen: true),
+//        ReservationSlot(time: "12:45 ~ 12:50", isOpen: true),
+        ReservationSlot(time: "13:05 ~ 13:10", isOpen: true),
+        ReservationSlot(time: "13:15 ~ 13:20", isOpen: true),
+//        ReservationSlot(time: "13:30 ~ 13:35", isOpen: true),
+        ReservationSlot(time: "13:45 ~ 13:50", isOpen: true),
+        ReservationSlot(time: "14:05 ~ 14:10", isOpen: true),
+        ReservationSlot(time: "14:15 ~ 14:20", isOpen: true),
+        ReservationSlot(time: "14:30 ~ 14:35", isOpen: true),
+        ReservationSlot(time: "14:45 ~ 14:50", isOpen: true),
+        ReservationSlot(time: "15:05 ~ 15:10", isOpen: true),
+        ReservationSlot(time: "15:15 ~ 15:20", isOpen: true),
+        ReservationSlot(time: "15:30 ~ 15:35", isOpen: true),
+//        ReservationSlot(time: "15:45 ~ 15:50", isOpen: true),
+        ReservationSlot(time: "16:05 ~ 16:10", isOpen: true),
+        ReservationSlot(time: "16:15 ~ 16:20", isOpen: true),
+        ReservationSlot(time: "16:30 ~ 16:35", isOpen: true),
+        ReservationSlot(time: "16:45 ~ 16:50", isOpen: true),
+        ReservationSlot(time: "17:05 ~ 17:10", isOpen: true),
+        ReservationSlot(time: "17:15 ~ 17:20", isOpen: true),
+        ReservationSlot(time: "17:30 ~ 17:35", isOpen: true),
+        ReservationSlot(time: "17:45 ~ 17:50", isOpen: true),
+    ]
+    
+    static let testDates = [
+        "2023/08/25",
+        "2023/08/26",
+        "2023/08/27",
+        "2023/08/28",
+        "2023/08/29",
+        "2023/08/30",
+        "2023/08/31",
+        "2023/09/01",
+        "2023/09/02",
+    ]
+}
